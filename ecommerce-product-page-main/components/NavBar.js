8248e10595cbf78d0e966c2694ad3c9171ee90c3
Fatima <54324041+fatima03598@users.noreply.github.com>
2024-04-17 21:11:58 +0100

    updated with base files

---
+++ ecommerce-product-page-main/components/NavBar.js
@@ -0,0 +1,29 @@
+import stylesheet from "../public/stylesheets/components/navbar.css";
+import logo from "../images/logo.svg";
+
+const template = document.createElement("template");
+
+template.innerHTML = `
+    <div class="nav-bar">
+        <div class="nav-bar__logo">
+        </div>
+    </div>
+`;
+
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.adoptedStyleSheets = [stylesheet];
+
+    shadow.append(template.content.cloneNode(true));
+
+    const logoEl = shadow.querySelector(".nav-bar__logo");
+    const img = document.createElement("img");
+    img.src = logo;
+    img.alt = "Sneakers Logo";
+    logoEl.appendChild(img);
+  }
+}
+
+customElements.define("nav-bar", NavBar);
---
+++ ecommerce-product-page-main/global.js
@@ -0,0 +1,34 @@
+export const items = [
+  {
+    id: 1,
+    title: "Fall Limited Edition Sneakers",
+    description:
+      "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+    price: 250,
+    sale: {
+      onSale: true,
+      discount: 50,
+    },
+    images: [
+      {
+        thumbnail: "./images/image-product-1-thumbnail.jpg",
+        full: "./images/image-product-1.jpg",
+      },
+      {
+        thumbnail: "./images/image-product-2-thumbnail.jpg",
+        full: "./images/image-product-2.jpg",
+      },
+      {
+        thumbnail: "./images/image-product-3-thumbnail.jpg",
+        full: "./images/image-product-3.jpg",
+      },
+      {
+        thumbnail: "./images/image-product-4-thumbnail.jpg",
+        full: "./images/image-product-4.jpg",
+      },
+    ],
+    company: "Sneaker Company",
+  },
+];
+
+export const cart = [];
