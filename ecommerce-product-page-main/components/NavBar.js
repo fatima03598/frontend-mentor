e273d909829f1449bdc04ad7693b2f542ebaf5bf
Fatima <54324041+fatima03598@users.noreply.github.com>
2024-05-14 09:29:48 +0100

    trainign updates

--- ecommerce-product-page-main/components/NavBar.js
+++ ecommerce-product-page-main/components/NavBar.js
@@ -1,29 +1,79 @@
-import stylesheet from "../public/stylesheets/components/navbar.css";
-import logo from "../images/logo.svg";
-
 const template = document.createElement("template");
 
 template.innerHTML = `
+    <link type="css" rel="stylesheet" href="../public/stylesheets/components/navbar.css" />
+
     <div class="nav-bar">
+        <div class="nav-bar__menu_mobile">
+        moooooo
+        </div>
         <div class="nav-bar__logo">
-        <slot name="logo"></slot>
+          <slot name="logo"></slot>
+        </div>
+        <div class="nav-bar__menu">
+        mmmmmmm
+        </div>
+        <div class="nav-bar__cart">
+          <slot name="cart"></slot>
+        </div>
+        <div class="nav-bar__user">
+          <slot name="user-pic"></slot>
         </div>
     </div>
 `;
 
+const debounce = (func) => {
+  let timer;
+
+  return function (event) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func, 100, event);
+  };
+};
 class NavBar extends HTMLElement {
   constructor() {
     super();
+
+    const menuItems = ["Collections", "Men", "Women", "About", "Contact"];
+
     const shadow = this.attachShadow({ mode: "open" });
-    shadow.adoptedStyleSheets = [stylesheet];
 
     shadow.append(template.content.cloneNode(true));
 
-    const logoEl = shadow.querySelector(".nav-bar__logo");
-    const img = document.createElement("img");
-    img.src = logo;
-    img.alt = "Sneakers Logo";
-    logoEl.appendChild(img);
+    this.updateMenuVisibility();
+
+    window.addEventListener(
+      "resize",
+      debounce((e) => {
+        this.handleWindowResize();
+      })
+    );
+  }
+
+  updateMenuVisibility() {
+    const isMobile = this.isMobile();
+
+    const mobileMenu = this.shadowRoot.querySelector(".nav-bar__menu_mobile");
+    const desktopMenu = this.shadowRoot.querySelector(".nav-bar__menu");
+
+    if (isMobile) {
+      mobileMenu.style.visibility = "visible";
+      desktopMenu.style.visibility = "hidden";
+    } else {
+      mobileMenu.style.visibility = "hidden";
+      desktopMenu.style.visibility = "visible";
+    }
+  }
+
+  handleWindowResize() {
+    this.updateMenuVisibility();
+  }
+
+  isMobile() {
+    return (
+      Math.min(window.screen.width, window.screen.height) < 768 ||
+      navigator.userAgent.indexOf("Mobi") > -1
+    );
   }
 }
 
