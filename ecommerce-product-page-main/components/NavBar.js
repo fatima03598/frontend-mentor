9ad9eb03aff521cd8d969df6da66057e6860370e
Fatima <54324041+fatima03598@users.noreply.github.com>
2024-05-21 19:45:05 +0100

    updated mobile menu

--- ecommerce-product-page-main/components/NavBar.js
+++ ecommerce-product-page-main/components/NavBar.js
@@ -4,8 +4,14 @@ template.innerHTML = `
     <link type="text/css" rel="stylesheet" href="./public/stylesheets/components/navbar.css" />
 
     <div class="nav-bar">
+        <div class="overflow"></div> 
         <div class="nav-bar__menu_mobile">
-        moooooo
+          <slot name="mobile-menu-icon"></slot>
+        </div>
+        <div class="nav-bar__full_menu_mobile">
+          <div class="nav-bar__full_menu_mobile_close_icon">
+            <slot name="mobile-menu-close-icon"></slot>
+          </div>
         </div>
         <div class="nav-bar__logo">
           <slot name="logo"></slot>
@@ -37,28 +43,81 @@ class NavBar extends HTMLElement {
   constructor() {
     super();
 
-    const menuItems = ["Collections", "Men", "Women", "About", "Contact"];
-
     const shadow = this.attachShadow({ mode: "open" });
 
     shadow.append(template.content.cloneNode(true));
 
-    this.addMenuItems(menuItems);
+    this.addMenuItems("nav-bar__menu", "nav-bar__menu-item");
+
+    this.mobileMenuMenuSetup();
   }
+
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === "mobile") {
       const trueValue = newValue === "true";
       this.updateMenuVisibility(trueValue);
+      this.mobileMenuMenuSetupEvents(trueValue);
     }
   }
 
-  addMenuItems(menuItems) {
-    const menu = this.shadowRoot.querySelector(".nav-bar__menu");
+  addMenuItems(parentElementSelector, itemClass) {
+    const menuItems = ["Collections", "Men", "Women", "About", "Contact"];
+
+    const menu = this.shadowRoot.querySelector("." + parentElementSelector);
     menuItems.forEach((item) => {
       const menuItem = document.createElement("div");
-      menuItem.classList.add("nav-bar__menu-item");
+      menuItem.classList.add(itemClass);
       menuItem.textContent = item;
       menu.appendChild(menuItem);
+    });
+  }
+
+  mobileMenuMenuSetup() {
+    const expandedMenu = this.shadowRoot.querySelector(
+      ".nav-bar__full_menu_mobile"
+    );
+    const mobileMenuItems = document.createElement("div");
+    mobileMenuItems.classList.add("mobile-menu-items");
+
+    expandedMenu.appendChild(mobileMenuItems);
+
+    this.addMenuItems("mobile-menu-items", "mobile-menu-item");
+  }
+
+  mobileMenuMenuSetupEvents(isMobile) {
+    console.log("mobile menu setup");
+    // hide expanded menu
+    const expandedMenu = this.shadowRoot.querySelector(
+      ".nav-bar__full_menu_mobile"
+    );
+
+    const overflow = this.shadowRoot.querySelector(".overflow");
+
+    if (!isMobile) {
+      expandedMenu.style.display = "none";
+    } else {
+      expandedMenu.style.display = "flex";
+      expandedMenu.classList.remove("active");
+    }
+
+    // add event listener to mobile menu icon
+    const mobileMenuIcon = this.shadowRoot.querySelector(
+      ".nav-bar__menu_mobile"
+    );
+
+    mobileMenuIcon.addEventListener("click", () => {
+      expandedMenu.classList.add("active");
+      overflow.classList.add("active");
+    });
+
+    // add event listener to close icon
+    const closeIcon = this.shadowRoot.querySelector(
+      ".nav-bar__full_menu_mobile_close_icon"
+    );
+
+    closeIcon.addEventListener("click", () => {
+      expandedMenu.classList.remove("active");
+      overflow.classList.remove("active");
     });
   }
 
@@ -67,23 +126,12 @@ class NavBar extends HTMLElement {
     const desktopMenu = this.shadowRoot.querySelector(".nav-bar__menu");
 
     if (isMobile) {
-      mobileMenu.style.display = "block";
+      mobileMenu.style.display = "flex";
       desktopMenu.style.display = "none";
     } else {
       mobileMenu.style.display = "none";
       desktopMenu.style.display = "flex";
     }
-  }
-
-  handleWindowResize() {
-    this.updateMenuVisibility();
-  }
-
-  isMobile() {
-    return (
-      Math.min(window.screen.width, window.screen.height) < 768 ||
-      navigator.userAgent.indexOf("Mobi") > -1
-    );
   }
 }
 
